cb61c0644f00983ff4843e4c569034f8cbda0d87
startxfr <clarue@startx.fr>
2017-12-30 17:24:58 +0100

    enable squash of docker images

--- travis-test.sh
+++ travis-test.sh
@@ -4,6 +4,10 @@ echo "=================> STARTING TEST"
 echo "=================> SETUP TEST ENVIRONMENT"
 set -ev
 
+echo "INFO: Updating docker configuration (experimental)"
+echo '{ "experimental": true, "storage-driver": "overlay2", "max-concurrent-downloads": 50, "max-concurrent-uploads": 50 }' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+
 echo "========> BUILDING APPLICATIONS Containers (dev)"
 sudo docker-compose build
 
